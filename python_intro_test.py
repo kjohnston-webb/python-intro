378ddb8948bdc76020b13070a1a4c93aa4d8718d
kjohnston-webb <kjohnston@webb.org>
2026-07-06 09:37:39 -0700

    Add files via upload

---
+++ python_intro_test.py
@@ -0,0 +1,6 @@
+from Python_Introduction import *
+
+def test_five_ave():
+    assert five_ave(1, 2, 3, 4, 5) == 3
+    assert five_ave(2, 1, 3, 4, 5) == 3
+    assert five_ave(20, 10, 30, 40, 50) == 30
